d20af9c799e7f29e8423ca4cc2d0051a1d1dd641
sarvesh talekar <sarveshtalekar123@gmail.com>
2024-12-06 19:41:40 +0530

    Add .gitignore and stop tracking dist folder

--- dist/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { allUsers, deleteUserByID, getUserByID, newUser } from '../controllers/user.controller.js';
-import { adminOnly } from '../middlewares/auth.js';
-const router = express.Router();
-router.post("/new-user", newUser);
-router.get("/all", adminOnly, allUsers);
-// router.get("/all", allUsers)
-router.route("/:id").get(getUserByID).delete(deleteUserByID);
-export { router as userRoutes };
